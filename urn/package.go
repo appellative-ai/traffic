22b61e3dbd394e5faf4e61c6e4f4816602e09871
advanced-go <mark.wilson.go@outlook.com>
2025-04-01 05:52:28 -0500

    Create package.go

---
+++ urn/package.go
@@ -0,0 +1,7 @@
+package urn
+
+const (
+	ResiliencyThreshold      = "resiliency:type/operative/agent/threshold"
+	ResiliencyInterpret      = "resiliency:type/operative/agent/interpret"
+	ResiliencyTrafficProfile = "resiliency:type/domain/metrics/profile"
+)
