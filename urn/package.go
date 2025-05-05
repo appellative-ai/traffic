c82a14dec128229cb95c753c29241143a804fb61
advanced-go <mark.wilson.go@outlook.com>
2025-05-05 14:59:46 -0500

    Update package.go

--- urn/package.go
+++ urn/package.go
@@ -1,6 +1,6 @@
 package urn
 
 const (
-	LimiterAgent  = "unn:behavioral-ai.github.com:resiliency:agent/traffic/rate-limiting"
-	RedirectAgent = "unn:behavioral-ai.github.com:resiliency:agent/traffic/redirect"
+	LimiterAgent  = "resiliency:agent/request/rate-limiting"
+	RedirectAgent = "resiliency:agent/request/redirect"
 )
