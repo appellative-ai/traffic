41010e6fdb6656e495f690d336432f4c420d5d28
advanced-go <mark.wilson.go@outlook.com>
2025-06-10 05:14:34 -0500

    Update package.go

--- module/package.go
+++ module/package.go
@@ -4,12 +4,14 @@ import (
 	"github.com/behavioral-ai/traffic/cache"
 	"github.com/behavioral-ai/traffic/limiter"
 	"github.com/behavioral-ai/traffic/redirect"
+	"github.com/behavioral-ai/traffic/routing"
 )
 
 var (
 	LimiterNamespaceName  = limiter.NamespaceName
 	RedirectNamespaceName = redirect.NamespaceName
 	CacheNamespaceName    = cache.NamespaceName
+	RoutingNamespaceName  = routing.NamespaceName
 )
 
 func Resolve(name string) (bool, any) {
