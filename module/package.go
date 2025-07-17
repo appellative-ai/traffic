f01b64c897515ada79c9677711df7e08f32217a1
advanced-go <mark.wilson.go@outlook.com>
2025-07-17 14:36:00 -0500

    Update package.go

--- module/package.go
+++ module/package.go
@@ -1,13 +1,17 @@
 package module
 
 import (
+	"github.com/appellative-ai/traffic/access"
+	"github.com/appellative-ai/traffic/authorization"
 	"github.com/appellative-ai/traffic/cache"
 	"github.com/appellative-ai/traffic/limiter"
 	"github.com/appellative-ai/traffic/routing"
 )
 
 var (
-	LimiterNamespaceName = limiter.NamespaceName
-	CacheNamespaceName   = cache.NamespaceName
-	RoutingNamespaceName = routing.NamespaceName
+	LimiterNamespaceName       = limiter.NamespaceName
+	CacheNamespaceName         = cache.NamespaceName
+	RoutingNamespaceName       = routing.NamespaceName
+	AccessNamespaceName        = access.NamespaceName
+	AuthorizationNamespaceName = authorization.NamespaceName
 )
