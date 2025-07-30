becf226834a6a186f6510571cfdc781574c29316
appellative-ai <mark.wilson.ai@outlook.com>
2025-07-30 13:52:09 -0500

    Update package.go

--- module/package.go
+++ module/package.go
@@ -1,10 +1,10 @@
 package module
 
 import (
-	"github.com/appellative-ai/traffic/access"
 	"github.com/appellative-ai/traffic/authorization"
 	"github.com/appellative-ai/traffic/cache"
 	"github.com/appellative-ai/traffic/limiter"
+	"github.com/appellative-ai/traffic/logger"
 	"github.com/appellative-ai/traffic/routing"
 )
 
@@ -12,6 +12,6 @@ var (
 	LimiterNamespaceName       = limiter.NamespaceName
 	CacheNamespaceName         = cache.NamespaceName
 	RoutingNamespaceName       = routing.NamespaceName
-	AccessNamespaceName        = access.NamespaceName
+	LoggerNamespaceName        = logger.NamespaceName
 	AuthorizationNamespaceName = authorization.NamespaceName
 )
